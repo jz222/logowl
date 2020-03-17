f5f422d0c4a26caf2b1a52563e5373374663f577
Timo <timotheus.zimmermann@me.com>
2020-03-17 17:57:10 +0100

    change key

--- models/user.go
+++ models/user.go
@@ -8,7 +8,7 @@ import (
 )
 
 type User struct {
-	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ID             primitive.ObjectID `json:"userId,omitempty" bson:"_id,omitempty"`
 	FirstName      string             `json:"firstName" bson:"firstName"`
 	LastName       string             `json:"lastName" bson:"lastName"`
 	Email          string             `json:"email" bson:"email"`
