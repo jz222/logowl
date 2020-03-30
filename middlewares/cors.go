96c03caf0d0f3d8fc673815c3e13d54c46933051
Timo <timotheus.zimmermann@me.com>
2020-03-30 17:54:30 +0200

    replace custom CORS middleware

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,17 +1,19 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
 
-func Cors(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, PUT, OPTIONS")
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
-	if c.Request.Method == "OPTIONS" {
-		c.AbortWithStatus(204)
-		return
-	}
-
-	c.Next()
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Type", "Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
 }
