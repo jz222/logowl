f961c6f18801be581ddfd03f89efcb5e89f3a029
Timo <timotheus.zimmermann@me.com>
2020-04-06 15:49:11 +0200

    update build script and readme

--- build.dev.sh
+++ build.dev.sh
@@ -1,3 +1,3 @@
-env CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o build/loggy .
+env CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o build/loggy ./cmd/loggy/
 docker build -t loggy .
 rm -rf build/
