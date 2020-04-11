008253d8da0d08685d1dc4f74d5cafe60632edcb
Timo <timotheus.zimmermann@me.com>
2020-04-11 20:02:34 +0200

    add get user unit test

--- internal/unittests/controller_test.go
+++ internal/unittests/controller_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jz222/loggy/internal/controllers"
+	"github.com/jz222/loggy/internal/mocks"
 )
 
 func TestRegisterError(t *testing.T) {
@@ -21,6 +22,26 @@ func TestRegisterError(t *testing.T) {
 	r.POST("/", loggingControllers.RegisterError)
 
 	c.Request, _ = http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte("{}")))
+
+	r.ServeHTTP(w, c.Request)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d, got %d", http.StatusOK, w.Code)
+	}
+}
+
+func TestGetUserInformation(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	userControllers := controllers.GetUserControllerMock()
+
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.Use(mocks.VerifyUserJWT)
+	r.GET("/", userControllers.Get)
+
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
 
 	r.ServeHTTP(w, c.Request)
 
