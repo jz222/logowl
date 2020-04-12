8e279625faa816895727db57d1cb6c2ff931078c
Timo <timotheus.zimmermann@me.com>
2020-04-12 11:45:24 +0200

    add unit test for generting tickets

--- internal/unittests/utils_test.go
+++ internal/unittests/utils_test.go
@@ -20,3 +20,14 @@ func TestFormatTimestamp(t *testing.T) {
 		t.Errorf("Timestamp was incorred, got: %d, expected: %d", convertedTimestamp, 1585872000)
 	}
 }
+
+func TestGenerateTicket(t *testing.T) {
+	ticket, err := utils.GenerateTicket()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if len(ticket) != 50 {
+		t.Errorf("Expected ticket to have %d characters, got %d", 50, len(ticket))
+	}
+}
