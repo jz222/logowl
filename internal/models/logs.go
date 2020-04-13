7354a79168d2332702aa6c19f2d4557d46fd9912
Timo <timotheus.zimmermann@me.com>
2020-04-13 17:32:45 +0200

    add user interaction

--- internal/models/logs.go
+++ internal/models/logs.go
@@ -27,30 +27,39 @@ type Metrics struct {
 	IsMobile string `json:"isMobile,omitempty" bson:"isMobile,omitempty"`
 }
 
+type UserInteraction struct {
+	Timestamp int64    `json:"timestamp" bson:"timestamp"`
+	Element   string   `json:"element" bson:"element"`
+	OuterHTML string   `json:"outerHtml" bson:"outerHtml"`
+	ElementID string   `json:"elementId" bson:"elementId"`
+	Path      []string `json:"path" bson:"path"`
+}
+
 // Error contains all properties of an error event.
 type Error struct {
-	ID          *primitive.ObjectID  `json:"id,omitempty" bson:"_id,omitempty"`
-	Message     string               `json:"message" bson:"message"`
-	Stacktrace  string               `json:"stacktrace" bson:"stacktrace"`
-	Evolution   map[string]int       `json:"evolution" bson:"evolution"`
-	Path        string               `json:"path" bson:"path"`
-	Line        string               `json:"line" bson:"line"`
-	Type        string               `json:"type" bson:"type"`
-	Adapter     Adapter              `json:"adapter" bson:"adapter"`
-	Fingerprint string               `json:"fingerprint" bson:"fingerprint"`
-	Badges      map[string]string    `json:"badges,omitempty" bson:"badges,omitempty"`
-	Snippet     map[string]string    `json:"snippet,omitempty" bson:"snippet,omitempty"`
-	Logs        []Logs               `json:"logs,omitempty" bson:"logs,omitempty"`
-	Ticket      string               `json:"ticket" bson:"ticket"`
-	Host        string               `json:"host" bson:"host"`
-	UserAgent   string               `json:"userAgent" bson:"userAgent"`
-	Metrics     Metrics              `json:"metrics" bson:"metrics"`
-	ClientIP    string               `json:"clientIp" bson:"clientIp"`
-	Count       int                  `json:"count,omitempty" bson:"count,omitempty"`
-	Timestamp   int64                `json:"timestamp" bson:"timestamp"`
-	Resolved    bool                 `json:"resolved" bson:"resolved"`
-	SeenBy      []primitive.ObjectID `json:"seenBy,omitempty" bson:"seenBy,omitempty"`
-	LastSeen    int64                `json:"lastSeen" bson:"lastSeen"`
-	CreatedAt   time.Time            `json:"createdAt" bson:"createdAt"`
-	UpdatedAt   time.Time            `json:"updatedAt" bson:"updatedAt"`
+	ID              *primitive.ObjectID  `json:"id,omitempty" bson:"_id,omitempty"`
+	Message         string               `json:"message" bson:"message"`
+	Stacktrace      string               `json:"stacktrace" bson:"stacktrace"`
+	Evolution       map[string]int       `json:"evolution" bson:"evolution"`
+	Path            string               `json:"path" bson:"path"`
+	Line            string               `json:"line" bson:"line"`
+	Type            string               `json:"type" bson:"type"`
+	Adapter         Adapter              `json:"adapter" bson:"adapter"`
+	Fingerprint     string               `json:"fingerprint" bson:"fingerprint"`
+	Badges          map[string]string    `json:"badges,omitempty" bson:"badges,omitempty"`
+	Snippet         map[string]string    `json:"snippet,omitempty" bson:"snippet,omitempty"`
+	Logs            []Logs               `json:"logs,omitempty" bson:"logs,omitempty"`
+	Ticket          string               `json:"ticket" bson:"ticket"`
+	Host            string               `json:"host" bson:"host"`
+	UserAgent       string               `json:"userAgent" bson:"userAgent"`
+	Metrics         Metrics              `json:"metrics" bson:"metrics"`
+	UserInteraction []UserInteraction    `json:"userInteraction" bson:"userInteraction"`
+	ClientIP        string               `json:"clientIp" bson:"clientIp"`
+	Count           int                  `json:"count,omitempty" bson:"count,omitempty"`
+	Timestamp       int64                `json:"timestamp" bson:"timestamp"`
+	Resolved        bool                 `json:"resolved" bson:"resolved"`
+	SeenBy          []primitive.ObjectID `json:"seenBy,omitempty" bson:"seenBy,omitempty"`
+	LastSeen        int64                `json:"lastSeen" bson:"lastSeen"`
+	CreatedAt       time.Time            `json:"createdAt" bson:"createdAt"`
+	UpdatedAt       time.Time            `json:"updatedAt" bson:"updatedAt"`
 }
