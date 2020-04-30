b1776759c7c8bfcda171543f8a0f090300d5da5f
Timo <timotheus.zimmermann@me.com>
2020-04-30 16:23:57 +0200

    add environment variable to set the request limit

--- internal/models/keys.go
+++ internal/models/keys.go
@@ -2,10 +2,11 @@ package models
 
 // Keys contains all environment variables.
 type Keys struct {
-	MONGO_URI     string
-	MONGO_DB_NAME string
-	PORT          string
-	SECRET        string
-	CLIENT_URL    string
-	IS_SELFHOSTED bool
+	MONGO_URI              string
+	MONGO_DB_NAME          string
+	PORT                   string
+	SECRET                 string
+	CLIENT_URL             string
+	TOTAL_MONTHLY_REQUESTS int
+	IS_SELFHOSTED          bool
 }
